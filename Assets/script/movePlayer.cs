ef566921dd542551279e95c4f5d31cf02820b96e
Mixmule <vkolmyko1976@gmail.com>
2019-02-24 22:58:47 +0200

    zaebalo

--- Assets/script/movePlayer.cs
+++ Assets/script/movePlayer.cs
@@ -7,9 +7,9 @@ public class movePlayer : MonoBehaviour
     public float hspeed = 3f;
     public float vspeed = 1f;
     public Animator anim;
-
     void Update()
     {
+
         float hspeedn = hspeed;
         float vspeedn = vspeed;
         if (Input.GetKey(KeyCode.LeftShift))
@@ -20,7 +20,7 @@ public class movePlayer : MonoBehaviour
         if (Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.LeftArrow))
             transform.position += new Vector3(hspeedn * Time.deltaTime, 0, 0);
         if (Input.GetKey(KeyCode.LeftArrow))
-            transform.position -= new Vector3(hspeedn * Time.deltaTime, 0, 0);
+           transform.position -= new Vector3(hspeedn * Time.deltaTime, 0, 0);
         if (Input.GetKey(KeyCode.DownArrow) && !Input.GetKey(KeyCode.UpArrow))
            transform.position -= new Vector3(0, vspeedn * Time.deltaTime, 0);
         if (Input.GetKey(KeyCode.UpArrow) )
